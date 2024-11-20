86a479cba34246cda6441de83847a226805adb6d
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-20 12:43:07 +0100

    add the server in the app.js and the link to the router

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express'); //importiamo express
+const app =  express(); //andiamo a creare un server
+const port = 3000; //attribuiamo una porta
+const postRouter = require('./router/posts.js')
+
+app.use(express.static('public'));//rendiamo il contenuto della cartella public publico
+
+app.listen(port, ()=>{ //mettiamo il server in ascolto alla porta numero 3000
+    console.log(`in ascolto sulla porta ${port}`)
+})
+
+app.use('/posts', postRouter)
