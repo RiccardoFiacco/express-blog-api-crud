1c8b41dd47f7d9427f9dc11e4684db70bb5c3327
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-20 12:43:38 +0100

    cerate the base for the controller

---
+++ controller/postController.js
@@ -0,0 +1,10 @@
+const posts = require('../data/posts.js'); //importiamo i post
+
+function index(req, res){}
+function show(req, res){}
+function store(req, res){}
+function update(req, res){}
+function modify(req, res){}
+function destroy(req, res){}
+
+module.exports={index, show, store, update, modify, destroy}
