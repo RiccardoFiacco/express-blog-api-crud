e049d589bea95af21c6d3f99f5aacfa400ca3c0e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-22 12:44:42 +0100

    import and add the middleware to check the error

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express'); //importiamo express
 const app =  express(); //andiamo a creare un server
 const port = 3000; //attribuiamo una porta
 const postRouter = require('./router/posts.js')
+const errorH = require('./middlewere/errorsHandler.js')
+const notFound = require('./middlewere/notFoundHandler.js')
 
 app.use(express.static('public'));//rendiamo il contenuto della cartella public publico
 app.use(express.json())//andiamo a rendere express in grado di leggere json come input
 app.listen(port, ()=>{ //mettiamo il server in ascolto alla porta numero 3000
     console.log(`in ascolto sulla porta ${port}`)
 })
+
+app.use(errorH)
+app.use(notFound)
 
 app.use('/posts', postRouter)
