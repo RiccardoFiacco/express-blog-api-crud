64fa185b18b565da3efe3b08dd5e0ef59ecaae91
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-22 13:03:56 +0100

    add correction

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express'); //importiamo express
 const app =  express(); //andiamo a creare un server
 const port = 3000; //attribuiamo una porta
-const postRouter = require('./router/posts.js')
-const errorH = require('./middlewere/errorsHandler.js')
-const notFound = require('./middlewere/notFoundHandler.js')
+const postRouter = require('./router/posts.js') //import il ruoter
+const errorH = require('./middlewere/errorsHandler.js') //importo la funzione per la gestione degli errori
+const notFound = require('./middlewere/notFoundHandler.js')//importo la funzione per la gestione della pagina non trovata
 
 app.use(express.static('public'));//rendiamo il contenuto della cartella public publico
 app.use(express.json())//andiamo a rendere express in grado di leggere json come input
@@ -11,7 +11,11 @@ app.listen(port, ()=>{ //mettiamo il server in ascolto alla porta numero 3000
     console.log(`in ascolto sulla porta ${port}`)
 })
 
-app.use(errorH)
-app.use(notFound)
+app.use("/post/:id", (req, res, next) => {
+    console.log("'Request Type:', req.method")
+    next()
+})
 
 app.use('/posts', postRouter)
+app.use(errorH)
+app.use(notFound)
